1e0f0bb5f29d6ce988c851889cc718cef4531736
Sellsword9 <yerayromeroortiz@gmail.com>
2023-03-30 08:41:36 +0200

    Deletion of MDL

--- mdl1.sql
+++
@@ -1,7 +0,0 @@
-/* -- Ej 1: Insertar una nueva oficina en Granada
- INSERT INTO oficina(codigo_oficina,ciudad, pais, region, codigo_postal, telefono, linea_direccion1)
- VALUES
- ('Cod_exp', 'Granada', 'Spain', 'Andalucia', '18003', '001001001', 'DirDeEjemplo')              
-*/
--- Ej 2: Insertar un empleado para la oficina de Granada que sea representante de ventas.
-INSERT INTO empleado VALUES (32, 'Yeray', 'Romero', 'Ortiz', '9999', 'example@examplemail', 
