d2437dde27726f6a34f3079d434250787e3ec545
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-01 01:36:53 +0200

    Rust Firsts Commits (1)

--- Rust/Algoritmia/ComprobarNumeroPrimo/src/main.rs
+++ Rust/Algoritmia/ComprobarNumeroPrimo/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn primo(n: u32) -> bool {
+    let mut primo: bool = true;
+    if n <= 1 {
+        primo = false;
+    }
+    let raiz: (n as f64).sqrt() as u32; //Calcula la raíz cuadrada del número. (*1)
+    for i in 2..=raiz { //El bucle busca desde 2 (minimo numero primo) hasta la raiz
+        if n % i == 0 {
+            primo = false;
+        }
+    }
+    return primo;
 }
+
+fn main() {
+    println!("Introduce un numero primo:");
+    let mut input = String::new(); 
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    let num = input.trim().parse::<u32>().expect("Por favor introduce un entero positivo");
+
+    if is_prime(num) {
+        println!("Tu ganas. {} es, de hecho, un primo", num);
+    } else {
+        println!("No. {} no es primo", num);
+    }
+}
+//(*1) 
+//Calcular la raíz cuadrada tiene su motivo.
+//si x*x = n, 
+//ningún divisor de n puede ser mayor que x
+//salvo que exista ya uno menor que x
+
--- Rust/ProbandoRust/src/main.rs
+++ Rust/ProbandoRust/src/main.rs
@@ -5,16 +5,13 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 
 fn main() {
+    const NUMERO: i32 = 1_232_200;      //Constantes
+    const PI: f64 = 3.141596;
     println!("Dime tu nombre");
-    
-    //Inicia  mutable  identificador: 
-    let       mut      nombre:        
-    //tipo = valor
-    String = String::new();
 
-    // En una línea:
     let mut nombre: String = String::new();
+    io::stdin().read_line(&mut nombre) //Devuelve un enum que puede ser OK o Error
+        .expect("No ha podido leer el nombre")
 
-
-    println!("Hello, world!");
+    println!("Hola, {}", nombre.trim_end());
 }
