52c02bccea715ac40da0321c688371b592395ec8
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-01 00:57:39 +0200

    Rust Setup

---
+++ Rust/ProbandoRust/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unsed)]
+use std::io; 
+use rand::Rng; //Para generar numeros aleatorios
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+
+fn main() {
+    println!("Dime tu nombre");
+    
+    //Inicia  mutable  identificador: 
+    let       mut      nombre:        
+    //tipo = valor
+    String = String::new();
+
+    // En una línea:
+    let mut nombre: String = String::new();
+
+
+    println!("Hello, world!");
+}
