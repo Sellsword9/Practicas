42b0880de5b092eb2ed0dcba992f607c7820ba57
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-17 17:37:48 +0200

    Python Basic Prompts: Primer avance

---
+++ Python/Basic/P2.py
@@ -0,0 +1,19 @@
+# Se pregunta un numero y se dice si es primo o no
+def lista_divisores(x):
+    a = []
+    if not(x <= 1):
+        a = [a for a in range(2, x) if x%a==0]
+    return a
+
+filtro_primo = lambda n: not bool(lista_divisores(n))
+
+num = input("Introduce un numero: ")
+
+try:
+    numero = int(num)
+    if filtro_primo(numero):
+        print("Tu numero es primo")
+    else:
+        print("Tu numero no es primo")
+except ValueError:
+    print("Error: Ingresa un valor numérico válido.")
