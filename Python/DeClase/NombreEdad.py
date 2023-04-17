9d9fab5e511ef7104074836840759a91741906f1
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-17 12:13:20 +0200

    Ejercicio basico input/output

--- Python/DeClase/NombreEdad.py
+++ Python/DeClase/NombreEdad.py
@@ -2,8 +2,11 @@
 nombre = input("Dime tu nombre: ")
 edad=input("Dime tu edad: ")
 
+# Forma basica
 # print("Te llamas", nombre, "y tu edad es", edad)
+# Otra forma basica 
 # print("Te llamas {} y tu edad es {}".format(nombre, edad))
+# Forma de Java (solo string)
 # print("Te llamas " + nombre + " y tu edad es " + edad)
 
 print(f"Te llamas {nombre} y tu edad es {edad}")
