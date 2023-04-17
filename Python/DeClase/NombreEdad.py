173abb2767572046a9f38ae49e2b8296b8d60b1f
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-17 12:09:32 +0200

    Ejercicio basico de input/output

---
+++ Python/DeClase/NombreEdad.py
@@ -0,0 +1,9 @@
+# Vamos a hacer un programa que pregunta tu nombre y te saluda
+nombre = input("Dime tu nombre: ")
+edad=input("Dime tu edad: ")
+
+# print("Te llamas", nombre, "y tu edad es", edad)
+# print("Te llamas {} y tu edad es {}".format(nombre, edad))
+# print("Te llamas " + nombre + " y tu edad es " + edad)
+
+print(f"Te llamas {nombre} y tu edad es {edad}")
