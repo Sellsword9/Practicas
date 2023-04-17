b0b9cdf11c552fb2b2ab9ec414555498996a372d
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-17 12:35:27 +0200

    Probando las listas

---
+++ Python/DeClase/Cuadrados.py
@@ -0,0 +1,10 @@
+# Se busca conseguir los cuadrados de los numeros entre 100 y 500
+
+lista = [n*n for n in range(100, 500)]
+
+# print(lista) 
+# Sirve pero printea lineas muy largas
+
+for a in lista:
+    print(a)
+
