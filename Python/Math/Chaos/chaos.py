a6224c9c2f05f5e244d4bf52c0909dd2888ea26e
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-24 23:45:44 +0200

    Python Chaos Refractor

--- Python/Math/Chaos/chaos.py
+++ Python/Math/Chaos/chaos.py
@@ -1,23 +1,31 @@
-# Crea números aleatorios entre 0 y 1
+"""
+Crea números aleatorios entre 0 y 1
+"""
 # Parámetros
 R = 3.789 #Entre 3 y 4
 X0 = 0.5 #Entre 0...1 y 0.999...
 
 def formula(x: float):
+    """
+    Devuelve el resultado de la aplicación de la fórmula
+    """
     return R * x * (1 - x)
 
-def generate_chaos(n):
-    g = []
+def generate_chaos(repeticiones: int):
+    """
+    Genera los números aleatorios
+    """
+    num_list = []
     x = X0
-    for _ in range(n):
+    for _ in range(repeticiones):
         x = formula(x)
-        g.append(x)
-    return g
+        num_list.append(x)
+    return num_list
 
 # Generar los números per se
 numbers = generate_chaos(100)
 
-# Printear 
-print("Generated pseudo-random numbers: ")
+# Printear
+print("Numeros generados: ")
 for a in numbers:
     print(a)
