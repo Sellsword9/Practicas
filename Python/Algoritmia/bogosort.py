773cef4b259d656cb78a7a4f8e3f642f515546b5
Sellsword9 <yerayromeroortiz@gmail.com>
2023-04-24 21:25:20 +0200

    Py Bogosort EOL

--- Python/Algoritmia/bogosort.py
+++ Python/Algoritmia/bogosort.py
@@ -8,7 +8,30 @@ and how much time does bogosort take to sort the same list.
 """
 import random
 import time
+import sys
 
+def protector(iterations_now: int):
+    """
+    Ends the program if the iterations exceed 100 millions
+    and prints iterations conditionally.
+    """
+    # checks if the iterations are a multiple of 5 by functional programming
+    if not iterations_now % 5:
+        if iterations_now < 10^6:
+            if not iterations_now % 5000:
+                print("Iterations:", iterations_now, sep=" ")
+        else:
+            if iterations_now < 10000:
+                print("Iterations:", iterations_now, sep=" ")
+            else:
+                if not iterations_now % 500:
+                    print("Iterations:", iterations_now, sep=" ")
+
+
+    if iterations_now > 100000000:
+        print("The algorithm took too long to sort the list.")
+        print("Please try again with a smaller list.")
+        sys.exit()
 def get_random_list(length: int, border: int) -> list:
     """
     Returns a list of random integers of a given length and border.
@@ -19,7 +42,7 @@ def is_sorted(list_to_check: list) -> bool:
     """
     Returns True if the list is sorted in ascending order, False otherwise.
     """
-    return list_to_check == sorted(list)
+    return list_to_check == sorted(list_to_check)
 
 def quicksort(list_to_sort: list):
     """
@@ -27,7 +50,7 @@ def quicksort(list_to_sort: list):
     and the number of iterations it took to sort the list.
     """
     if len(list_to_sort) <= 1:
-        return list_to_sort
+        return list_to_sort, 0
     pivot = list_to_sort[random.randint(0, len(list_to_sort) - 1)]
     # to make it fair, we will do the quicksort using a for statement
     # instead of a list comprehension
@@ -43,8 +66,11 @@ def quicksort(list_to_sort: list):
         else:
             greater.append(element)
         iteraciones += 1
+        protector(iteraciones)
 
-    return quicksort(less) + equal + quicksort(greater), iteraciones
+    left, left_iterations = quicksort(less)
+    right, right_iterations = quicksort(greater)
+    return left + equal + right, iteraciones + left_iterations + right_iterations
 
 def main():
     """
@@ -56,24 +82,26 @@ def main():
 
         random_list = get_random_list(list_length, list_border)
         list_to_sort = random_list.copy()
-        print("Random list:", random_list, sep="/n")
+        list_to_sort2 = random_list.copy()
+        print("Random list:", random_list, sep="\n")
         start_time = time.time()
         temp_iterations = 0
-        _, temp_iterations = quicksort(list_to_sort)
+        list_to_sort, temp_iterations = quicksort(list_to_sort)
         end_time = time.time()
         duration = end_time - start_time
-        print("Quicksort:", random_list, sep="/n")
+        print("Quicksort:", list_to_sort, sep="\n")
         print("Time taken:", duration, "seconds", sep=" ")
         print("Iterations:", temp_iterations, sep=" ")
         # Resets the iterations counter
         temp_iterations = 0
         start_time = time.time()
-        while not is_sorted(list_to_sort):
-            random.shuffle(list_to_sort)
+        while not is_sorted(list_to_sort2):
+            random.shuffle(list_to_sort2)
             temp_iterations += 1
+            protector(temp_iterations)
         end_time = time.time()
         duration = end_time - start_time
-        print("Bogosort:", list_to_sort, sep="/n")
+        print("Bogosort:", list_to_sort2, sep="\n")
         print("Time taken:", duration, "seconds", sep=" ")
         print("Iterations:", temp_iterations, sep=" ")
 
@@ -81,5 +109,5 @@ def main():
     except ValueError:
         print("Invalid input.")
         main()
-    # Executes the main function
-    main()
+# Executes the main function
+main()
