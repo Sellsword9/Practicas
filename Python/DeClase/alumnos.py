ba0dd1432cddb01f0babae2caf7f0335545e2854
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-02 13:56:14 +0200

    Hecho en clase

---
+++ Python/DeClase/alumnos.py
@@ -0,0 +1,19 @@
+"""
+Este programa tiene una lista rellenada con información de varios alumnos
+Nos mostrará un menú con el que podremos añadir un nuevo alumno y ver los
+datos de los alumnos que ya hay
+"""
+
+alumnos = [
+    {"nombre" : "Ismael", "Nota" : 7.5, "Edad" : 20},
+    {"nombre" : "Pablo", "Nota" : 5, "Edad" : 18, "Direccion" :
+                                                                {
+                                                                    "Ciudad" : "Granada",
+                                                                    "Calle" : "C/Albondon",
+                                                                    "numero" : 10
+                                                                } 
+    },
+    {"nombre": "Yeray", "nota":7.15}
+    
+    
+]
