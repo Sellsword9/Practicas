9d31ef64b79de3bb1919e3d1b73ee7901fbd3228
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-05 09:54:35 +0200

    No sabia que se podia hacer esto

---
+++ DeJava/Curiosidades/ForSinLlave.java
@@ -0,0 +1,8 @@
+public class ForSinLlave{
+    public static void main(String[] args) {
+        for (int i = 0; i<10; i++) 
+        System.out.println(i + 1);
+        
+        System.out.println("Un programador de Python estuvo aquí");
+    }
+}
