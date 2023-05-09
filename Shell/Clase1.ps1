d2627bde2eec3360896768d81bb039b25276a17e
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-09 08:53:09 +0200

    Borrados comms

--- Shell/Clase1.ps1
+++ Shell/Clase1.ps1
@@ -1,15 +1,5 @@
 ﻿# ----- Bucles
 # do while -- while -- do until -- for -- foreach 
-# $true 
-$opciones = "si Si SI sí Sí SÍ sÍ"
-# do {
-#    $resp = Read-Host "¿Desea salir?"
-#    $bol = $opciones.indexOf($resp) -lt 0
-#    if ($bol)
-#    {
-#        Write-Host "Has pulsado una opcion incorrecta"
-#    }
-# } while ($bol)
 clear
 $n = 0
 while ($n -lt 10) {
