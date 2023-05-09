11ff85385d572d338073a28ea2f378a77a1a0166
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-09 09:39:49 +0200

    Ejercicio practico de shell

---
+++ Shell/Clase1-2.ps1
@@ -0,0 +1,14 @@
+﻿# Ejecicio de práctica
+# Preguntar que de un numero hasta que sea uno entre el 1 y el 10
+$ValidNums = ""
+do
+{
+    $ValidNums += 1..10
+    $num = Read-Host "Introduce un numero"
+    $b = $ValidNums.Contains($num)
+    if (!$b)
+    {
+        Write-Host "Numero incorrecto"
+    }
+}
+until($b)
