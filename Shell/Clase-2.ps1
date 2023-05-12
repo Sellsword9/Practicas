c78f0ac1a6c155d0a50bb9d418845bf17ce41e9b
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-12 14:07:22 +0200

    Segunda clase

---
+++ Shell/Clase-2.ps1
@@ -0,0 +1,18 @@
+﻿clear
+function copiar($path1, $path2)
+{
+    #Función que copia los ficheros de un diretorio a otro
+
+    if (Test-Path -Path $directorio)
+        {
+            #TODO
+        }
+
+}
+function contar($path1)
+{
+    #Función que devuelve el numero de ficheros que hay en ese path
+    # TODO
+}
+
+$directorio = Read-Host "Introduzca directorio de destino"
