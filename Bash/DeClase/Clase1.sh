3f4041c1f1d14fe46b6f053f55cc715bb70d6f53
Sellsword9 <yerayromeroortiz@gmail.com>
2023-06-12 14:24:02 +0200

    Clase 1.1

--- Bash/DeClase/Clase1.sh
+++ Bash/DeClase/Clase1.sh
@@ -22,3 +22,19 @@ mv /ruta/*[]* /ruta2 # Mueve el fichero a la ruta2
 
 # Para comprimir
 tar -czvf copia.tar.gz /test/h.text
+# Para mover
+mv copia.tar.gz /test2
+##
+find /home/ -type f -name "*html" -size -500l -ctime -2 -exec ls -la {} \;
+##
+ip a | grep "inet " | grep "global" | tr -s " " "|" | cut -d "|" -f 3
+## 
+grep "^-"
+##
+grep -v "^-"
+##
+cut -d "|" -f5,9 | sort -nr -t "|" -k2 | head -1
+##
+ls -Rla
+##
+
